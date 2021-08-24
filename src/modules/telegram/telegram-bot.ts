2b6114785c11df91019ba6cbb69a8f495499bd1a
Leandro Rocha <alzeheimer@gmail.com>
2021-08-24 16:04:05 -0300

    added hello/goobye messages

--- src/modules/telegram/telegram-bot.ts
+++ src/modules/telegram/telegram-bot.ts
@@ -1,16 +1,37 @@
 import { Telegraf } from 'telegraf'
 import { requireEnv } from '../../core/util'
 import * as BotMenu from './telegram-menu'
+import * as os from 'os'
 
 console.debug('Starting TelegramBot')
 
 const bot = new Telegraf(requireEnv('BOT_TOKEN'))
 BotMenu.createMainMenu(bot)
 
-if (process.env.DEBUG == 'true') bot.on('sticker', (ctx) => { console.log(ctx.message.sticker) })
+if (process.env.DEBUG == 'true') {
+    bot.on('sticker', (ctx) => { console.log(ctx.message.sticker) })
 
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+    broadcast(`Hello! Running from ${os.hostname()}`)
+
+
+}
+
+process.once('SIGINT', async () => {
+    await broadcast('Bye!')
+    bot.stop('SIGINT')
+})
+process.once('SIGTERM', async () => {
+    await broadcast('Bye')
+    bot.stop('SIGTERM')
+})
 
 bot.launch()
 console.info('TelegramBot started')
+
+
+async function broadcast(message: string) {
+    const broadcastChatIds = process.env.BROADCAST_CHAT_IDS?.split(',')
+    if (!broadcastChatIds) return
+    for (const chatId of broadcastChatIds)
+        await bot.telegram.sendMessage(chatId, message)
+}
